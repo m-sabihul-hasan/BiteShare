962abd6c434baed60a48fce26b955a993a05bbfe
Sabih <sabih.ulhasan@yahoo.com>
2024-11-06 09:50:42 +0100

    Added Xcode files

---
+++ BiteShare/BiteShare/BiteShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteShareApp.swift
+//  BiteShare
+//
+//  Created by Muhammad Sabihul Hasan on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BiteShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
