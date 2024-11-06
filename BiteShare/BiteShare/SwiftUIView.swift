5f0c8f5b232c9b6191f6d6191286a0f0543a7636
Sabih <sabih.ulhasan@yahoo.com>
2024-11-06 11:47:43 +0100

    Added log in view

---
+++ BiteShare/BiteShare/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  BiteShare
+//
+//  Created by Muhammad Sabihul Hasan on 06/11/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
