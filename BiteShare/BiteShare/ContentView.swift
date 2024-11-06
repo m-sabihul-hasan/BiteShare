5f5ad82c6a61617fc3677a808060e5f35892fad4
Sabih <sabih.ulhasan@yahoo.com>
2024-11-06 09:41:08 +0100

    Added XCode Project

---
+++ BiteShare/BiteShare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BiteShare
+//
+//  Created by Muhammad Sabihul Hasan on 06/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
