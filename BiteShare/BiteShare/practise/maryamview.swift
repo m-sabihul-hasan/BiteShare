f760594d8be6cb83868ed6f8c2c7af81120cd97b
MaryamJeyhaninejad <MaryamJeyhani75@gmail.com>
2024-11-11 10:00:46 +0100

    added practise

---
+++ BiteShare/BiteShare/practise/maryamview.swift
@@ -0,0 +1,18 @@
+//
+//  maryamview.swift
+//  BiteShare
+//
+//  Created by Maryam Jeyhaninejad on 11/11/24.
+//
+
+import SwiftUI
+
+struct maryamview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    maryamview()
+}
